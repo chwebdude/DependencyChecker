2fc2961df006ab67063f285e8bbee844811aa12d
Fabrice Andréïs <7746357+chwebdude@users.noreply.github.com>
2019-03-04 18:01:42 +0100

    +semver: minor

--- DependencyChecker/Model/CodeProject.cs
+++ DependencyChecker/Model/CodeProject.cs
@@ -1,25 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DependencyChecker.Model
 {
     public class CodeProject
     {
-        public string Name
-        {
-            get;
-            set;
-        }
+        #region Properties
 
-        public string NuGetFile
-        {
-            get;
-            set;
-        }
+        public string Name { get; set; }
+
+        public string NuGetFile { get; set; }
 
         public List<PackageStatus> PackageStatuses { get; set; } = new List<PackageStatus>();
+
+        #endregion
     }
 }
