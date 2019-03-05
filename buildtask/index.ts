df2e189894c2b391f7add01f54f9b96fd989ee40
Fabrice Andréïs <7746357+chwebdude@users.noreply.github.com>
2019-03-05 12:03:49 +0100

    Swith to path selectors and include custom NuGet file

--- buildtask/index.ts
+++ buildtask/index.ts
@@ -8,18 +8,18 @@ async function run() {
 
     try {
 
-        var path = tl.getInput("path");
+        var path = tl.getPathInput("path");
         var searchRecursive = tl.getBoolInput("searchRecursive");
         var includePrerelease = tl.getBoolInput("includePrerelease", true);
         var createReport = tl.getBoolInput("createReport", true);
-        var reportPath = tl.getInput("reportPath");
+        var reportPath = tl.getPathInput("reportPath");
         var createBadge = tl.getBoolInput("createBadge");
-        var badgePath = tl.getInput("badgePath");
+        var badgePath = tl.getPathInput("badgePath");
         var style = tl.getInput("style");
+        var nuGetFile = tl.getPathInput("customNuGetfile", false);
 
         let toolPath = __dirname + "\\..\\bin\\DependencyChecker.exe";
 
-        // let arg = [] path + searchRecursive + includePrerelease + createReport + reportPath + createBadge + badgePath + style;
         let arg = ["--search-path", path, "--report-path", reportPath, "--badge-path", badgePath, "--badge-style", style];
         if (createReport)
             arg.push("--create-report")
@@ -29,6 +29,9 @@ async function run() {
             arg.push("--search-recursive")
         if (includePrerelease)
             arg.push("--prerelease")
+        if(nuGetFile != "")        
+            arg.push("--nuget-file", nuGetFile);
+        
 
         let tool: trm.ToolRunner = tl.tool(toolPath).arg(arg);
         let result: number = await tool.exec();
