42a1d366e390b6ee343efd9b536a1fe5935ddf80
Fabrice Andréïs <7746357+chwebdude@users.noreply.github.com>
2019-03-06 09:08:45 +0100

    Upgraded JS library

--- buildtask/index.ts
+++ buildtask/index.ts
@@ -1,5 +1,5 @@
-import tl = require('vsts-task-lib/task');
-import trm = require('vsts-task-lib/toolrunner');
+import tl = require('azure-pipelines-task-lib/task');
+import trm = require('azure-pipelines-task-lib/toolrunner');
 
 async function run() {
     if (process.platform != 'win32') {
