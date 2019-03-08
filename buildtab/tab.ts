0417abc98e28e2e6588a531ca512c6eba4f0e41c
Fabrice Andréïs <7746357+chwebdude@users.noreply.github.com>
2019-03-08 09:56:33 +0100

    Include mustache and render test content

--- buildtab/tab.ts
+++ buildtab/tab.ts
@@ -3,49 +3,62 @@ import VSS_Service = require("VSS/Service");
 import TFS_Build_Contracts = require("TFS/Build/Contracts");
 import TFS_Build_Extension_Contracts = require("TFS/Build/ExtensionContracts");
 import DT_Client = require("TFS/DistributedTask/TaskRestClient");
+import Mustache = require("mustache");
 
-export class InfoTab extends Controls.BaseControl {	
+export class InfoTab extends Controls.BaseControl {
 	constructor() {
 		super();
 	}
-		
+
 	public initialize(): void {
 		super.initialize();
 		// Get configuration that's shared between extension and the extension host
 		var sharedConfig: TFS_Build_Extension_Contracts.IBuildResultsViewExtensionConfig = VSS.getConfiguration();
 		var vsoContext = VSS.getWebContext();
 		console.log("vsoContext", vsoContext);
-		if(sharedConfig) {
+		if (sharedConfig) {
 			// register your extension with host through callback
 			sharedConfig.onBuildChanged((build: TFS_Build_Contracts.Build) => {
-				this._initBuildInfo(build);	
+				this._initBuildInfo(build);
 				console.log("build", build);
 				/*
 				* If any task uploaded some data using ##vso[task.addattachment] (https://github.com/Microsoft/vso-agent-tasks/blob/master/docs/authoring/commands.md)
 				* Then you could consume the data using taskclient
 				* sample code -
 				*/
-					var taskClient = DT_Client.getClient();
-					console.log("taskClient", taskClient);
-					taskClient.getPlanAttachments(vsoContext.project.id, "build", build.orchestrationPlan.planId, "dependcies_check_result").then((taskAttachments)=> {
-						$.each(taskAttachments, (index, taskAttachment) => {
-							if (taskAttachment._links && taskAttachment._links.self && taskAttachment._links.self.href) {
-								var link = taskAttachment._links.self.href;
-								console.log("link", link);
-								var attachmentName = taskAttachment.name;
-								console.log("attachmentName", attachmentName);
-								// do some thing here
-								// see how to get auth https://www.visualstudio.com/en-us/docs/report/analytics/building-extension-against-analytics-service
-							}
-						});
+				var taskClient = DT_Client.getClient();
+				console.log("taskClient", taskClient);
+				taskClient.getPlanAttachments(vsoContext.project.id, "build", build.orchestrationPlan.planId, "dependcies_check_result").then((taskAttachments) => {
+					$.each(taskAttachments, (index, taskAttachment) => {
+						if (taskAttachment._links && taskAttachment._links.self && taskAttachment._links.self.href) {
+							var link = taskAttachment._links.self.href;
+							console.log("link", link);
+							var attachmentName = taskAttachment.name;
+							console.log("attachmentName", attachmentName);
+							// do some thing here
+							// see how to get auth https://www.visualstudio.com/en-us/docs/report/analytics/building-extension-against-analytics-service
+							$.ajax({ url: link, })
+								.done(res => {
+									console.log(res);
+									var template = $("#template").html();
+									Mustache.parse(template);
+									var rendered = Mustache.render(template, {name: "TestName"});
+									$("#target").html(rendered);
+								})
+								.fail(error => {
+									console.error(error);
+								});
+
+						}
 					});
-				
+				});
+
 			});
-		}		
+		}
 	}
-	
+
 	private _initBuildInfo(build: TFS_Build_Contracts.Build) {
-		
+
 	}
 }
 
@@ -54,4 +67,3 @@ InfoTab.enhance(InfoTab, $(".build-info"), {});
 // Notify the parent frame that the host has been loaded
 VSS.notifyLoadSucceeded();
 
-	
