06b27b42a46a36d29c8c9ef69acd86a4c8733f09
Fabricio Ferreira <fabricio.ferreira@outlook.com>
2022-11-06 16:57:30 +1300

    Improved the local build process for testing

--- build.bat
+++ build.bat
@@ -1,1 +1,9 @@
+cd ./buildtab
+cmd /c "npm i && npm run build"
+cd ../buildtask
+cmd /c "npm i && npm run build"
+cd ..
+dotnet restore
+dotnet build -c Release --no-restore
+dotnet publish --no-build -o ./buildtask/bin 
 tfx extension create --manifest-globs vss-extension.json
